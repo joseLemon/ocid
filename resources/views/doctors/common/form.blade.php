188631a191c5e871466cd3348eaeb2e4a3bd09bd
Jose Angel <angel.lujan@orderos.mx>
2019-07-14 17:43:26 -0600

    fixed header

--- resources/views/doctors/common/form.blade.php
+++ resources/views/doctors/common/form.blade.php
@@ -584,7 +584,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Agregar cita</h5>
+                <h5 class="modal-title">Editar fecha</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
